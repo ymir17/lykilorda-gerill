519ea460a4ab9568a7c1a1d3d57309abbf7240eb
ymir17 <ymir17@ru.is>
2020-03-05 16:09:31 +0000

    fixed all that was broken, added loading functionality

--- public/js/index.js
+++ public/js/index.js
@@ -1,27 +1,32 @@
 $(document).ready(() => {
-    $(window).load(clearInp());
-    /*
-    How did jquery just stopped working!!?
-    */
-
-    $(".btn").click(() => {
-        console.log("That was a button!");
-    })
+    clearInp();
 
     $("#clear").click(() => {
-        console.log("Hi");
+        console.log("Clear button");
         clearInp();
     });
 
     $("#gen").click(() => {
-        console.log("sup");
+        console.log("Generate button");
+        addLoading();
         $.get('/getPW', (data, status) => {
+            remLoading();
             $("#inp").val(data);
         });
     });
 
     function clearInp() {
-        console.log("Only once");
+        console.log("Clear function");
         $("#inp").val('');
+    }
+
+    function addLoading() {
+        console.log("Loading");
+        $("#gen").html("<span class='fa fa-spinner fa-spin'></span>");
+    }
+
+    function remLoading() {
+        console.log("Not loading");
+        $("#gen").html("Framleiða");
     }
 });
